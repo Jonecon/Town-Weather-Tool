fe5857bff0328120dcfb547e7004fe270c141526
Connor Jones <connor.gauthern@gmail.com>
2020-06-04 08:03:40 +0000

    Trying to fix map breaking

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,4 @@
-//import React, { useState } from 'react';
-//import TownResponse from '../components/TownResponse';
-//import fetch from 'isomorphic-unfetch'
+import { TownResponse } from 'components/TownResponse.js';
 
 var map;
 var marker;
@@ -19,19 +17,11 @@ initMap = function() {
 	});
 }
 
-
-//const [responseData, setResponseData] = useState('');
-
-//const handleTownChange = async (townValue) => {
-// 	const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${townValue},nz`)
-//    const json = await res.json()
-//      	setResponseData(json);
-//    }
-
-//const clearResponse = () => {
-//  	setResponseData('');
-//}
-
-//goToTown = function(latLng) {
+goToTown = function(latLng){
+	var lat = latLng.lat;
+	var lng = latLng.lng;
 	
-//}
+	fetch("https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&lat=" + lat + "&lng=" + lng)
+	.then(response => response.json())
+	.then(json => TownResponse(json));
+}
